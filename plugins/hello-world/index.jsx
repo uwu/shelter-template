5e48d9c9c1a1909bf489a5ca2d1d3ae4da8f6dc6
ioj4 <69911332+ioj4@users.noreply.github.com>
2024-02-25 22:11:50 +0100

    Update index.jsx

--- plugins/hello-world/index.jsx
+++ plugins/hello-world/index.jsx
@@ -1,13 +1,12 @@
 const {
+	util: { log }
 } = shelter;
 
 export function onLoad() {
-	// optional
 	// you can safely run onLoad actions at the top level!
-	console.log("Hello, World from shelter!")
+	log("Hello, World from shelter!")
 }
 
 export function onUnload() {
-	// required
-	console.log("Goodbye, World from shelter!")
+	log("Goodbye, World from shelter!")
 }
